588f9840a0b085d603d539f9adcf09954065fdd5
bee1st <beebee0819@gmail.com>
2019-12-07 23:35:58 +0900

    edit sum

--- 3_5-sum.py
+++ 3_5-sum.py
@@ -1,4 +1,10 @@
-import sys
-x = list(map(int, sys.stdin.readline().split()))
+a, b, c, d, e, f, g = map(int, input().split())
 
-print(x[0] + x[1] + x[2] + x[3] + x[4] + x[5] + x[6])
+x = [a, b, c, d, e, f, g]
+
+y = 0
+
+for i in x:
+    y = i + y
+
+print(y)
---
+++ 3_8-1ton1.py
@@ -0,0 +1,6 @@
+a = int(input())
+i = 0
+
+for x in range(0, a):
+    i = x + 1 + i
+    print(x, i)
