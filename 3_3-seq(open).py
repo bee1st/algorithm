1764c13b729e3e9168f379d665f484cba401f139
bee1st <beebee0819@gmail.com>
2019-12-07 22:16:16 +0900

    stupid me

--- 3_3-seq(open).py
+++ 3_3-seq(open).py
@@ -9,7 +9,7 @@ c.sort()
 
 d = c[0]
 
-for d in range(c[0] -1, c[1], 1):
+for d in range(c[0] -1, c[1]):
     d = d + 1
     if d <= c[1]:
         print(d, end=' ')
---
+++ 3_6-seq2.py
@@ -0,0 +1,6 @@
+a = int(input())
+j = 0
+
+for i in range(1, a + 1):
+    j = i + j
+    print(f'{1}..{i} {j}')
