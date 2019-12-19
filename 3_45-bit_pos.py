ff92f6dbf6cd646d2a45f858fe93c206a25cf44e
bee1st <beebee0819@gmail.com>
2019-12-19 18:31:35 +0900

    solve number53

--- 3_45-bit_pos.py
+++ 3_45-bit_pos.py
@@ -1,13 +1,10 @@
 import sys
 x = list(map(int, sys.stdin.readline().split()))
 
-a = x[0]
+n = x[0]
+a = 0
+while True:
+    a = a + 1
 
-while a // 2 > 0:
-    print(a % 2)
-    a = a // 2
-    continue
 
-print(a % 2)
-
-# pass
+pass
--- 3_53-cha.py
+++ 3_53-cha.py
@@ -3,6 +3,12 @@ x = list(map(int, sys.stdin.readline().split()))
 
 a = x[0]
 b = x[1]
-
-for i in range(0, 10001):
-    
+c = 0
+print(a, b, end=' ')
+while True:
+    c = a - b
+    a = b
+    b = c
+    if c < 0:
+        break
+    print(c, end=' ')
