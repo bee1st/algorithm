eef0b55bde6b26dfdb6e7370de1779dc6bc798d2
bee1st <beebee0819@gmail.com>
2020-01-19 03:42:06 +0900

    very difficult sugar

--- 4_23-sugar.py
+++ 4_23-sugar.py
@@ -6,36 +6,38 @@ a = 0
 b = 0
 c = 0
 
-if 3 <= n < 5000:
-    if n % 5 == 0:
-        b = n // 5
-    elif n % 3 == 0:
-        c = n // 3
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-            if 5 * i + 3 * j == n:
-                a = i + j
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if 5 * i + 3 * j == n:
+            a = i + j
+            
+if n % 5 == 0:
+    b = n // 5
+elif n % 3 == 0:
+    c = n // 3
 
-if a == 0:
-    if b == 0:
-        print(c)
-    elif c == 0:
-        print(b)
-elif a != 0:
-    if a < b:
-        print(a)
-    elif a < c:
-        print(a)
-    elif b < a:
-        print(b)
-    elif c < a:
-        print(c)
-    elif b < c:
+
+if n == 4 or n == 7:
+    print('-1')
+     
+elif a == 0 and b == 0:
+    print(c)
+elif b == 0 and c == 0:
+    print(a)
+elif c == 0 and a == 0:
+    print(b)
+elif a == 0:
+    if b < c:
         print(b)
     elif c < b:
         print(c)
-else:
-    print('-1')
-
-pass
-
+elif b == 0:
+    if a < c:
+        print(a)
+    elif c < a:
+        print(c)
+elif c == 0:
+    if b < a:
+        print(b)
+    elif a < b:
+        print(a)
